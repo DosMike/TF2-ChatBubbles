652bb58ae38f20f7c4693da09e56a2e7244ffa41
DosMike <xanatosefan@gmx.de>
2021-11-13 10:56:34 +0100

    Updated dependencies and removed SPSauce binaries

--- sps
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-fname="spsauce/"$(ls -r1A spsauce | grep '^SPSauce-.*-all\.jar$' | head -n1)
-if [[ -f $fname ]]; then
-  java -jar $fname $@
-fi
--- sps.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-for /f "delims=" %%F in ('dir spsauce\SPSauce-*-all.jar /b /o-n') do set spsfile=%%F
-if "%spsfile%"=="" goto :EOF
-call java -jar spsauce\%spsfile% %*
